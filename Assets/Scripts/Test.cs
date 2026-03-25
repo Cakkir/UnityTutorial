b73dc91795209905da501c85214ec4b9fd779107
Cakkir <s230446018@stu.thk.edu.tr>
2026-03-25 17:11:59 +0300

    v0.001

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake() {
+        int score = 0;
+
+        print("Awake");
+        
+    }
+    
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        
+    }
+
+    void LateUpdate() {
+        
+    }
+}
